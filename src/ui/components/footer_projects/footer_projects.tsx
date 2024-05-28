d7cc6a0eef87034315fc6d3a89f671287e5b9be6
Taras M <tmaliukh@gmail.com>
2024-05-28 13:51:26 +0300

    add project page

--- src/ui/components/footer_projects/footer_projects.tsx
+++ src/ui/components/footer_projects/footer_projects.tsx
@@ -2,6 +2,9 @@
 
 import style from "./style.module.scss"
 import Email from "@/../public/svg/email.svg"
+import Telegram from "@/../public/svg/ic_telegram.svg"
+import Instagram from "@/../public/svg/ic_instagram.svg"
+import Behance from "@/../public/svg/ic_behance.svg"
 import Logo from "@/../public/svg/logo_white.svg"
 import Artevide from "@/../public/svg/artevide.svg"
 
@@ -9,19 +12,29 @@ export default function Footer_projects() {
 
     return (
         <div className={style.container}>
-            <div className={style.container_inner}>
-                <div>
-                    <div className={style.logo}>
+            <div className={style.logo}>
                         <Logo />
                     </div>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                    <div className={style.email}>
-                        <div className={style.email_text}>Lopem_ipsum@gmail.com</div>
-                        <div><Email fill={"#797979"} /></div>
+            <div className={style.container_inner}>
+                <div>
 
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                    <div className={style.social}>
+                        <div className={style.icons}>
+                            <div><Telegram fill={"#797979"} /></div>
+                            <div><Instagram fill={"#797979"} /></div>
+                            <div><Behance fill={"#797979"} /></div>
+                        </div>
+
+                        <div className={style.email}>
+                            <div className={style.email_text}>Lopem_ipsum@gmail.com</div>
+                            <div><Email fill={"#797979"} /></div>
+
+                        </div>
                     </div>
+
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
                     <div>
                         <div className={style.block}>
                             <div className={style.block1}>Lorem ipsum</div>
